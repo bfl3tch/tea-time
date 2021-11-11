ea526045ed3ea87dadb626b6053777f28a162a8b
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-11-10 22:32:44 -0500

    Create subscription model specs for assocations and validations

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_numericality_of(:price) }
+    it { should validate_presence_of(:frequency) }
+    it { should define_enum_for(:frequency).with_values(['Weekly', 'Bi-Weekly', 'Monthly']) }
+
+  end
+
+  describe 'associations' do
+    it { should have_many(:customer_subscriptions) }
+    it { should have_many(:customers).through(:customer_subscriptions) }
+    it { should have_many(:subscriptions).through(:customer_subscriptions) }
+  end
+end
