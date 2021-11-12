d1b29dfaa420149c4b905b1569d4a964f1172349
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-11-11 20:13:41 -0500

    Create restful routes with scope to account for nesting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
 namespace :api do
   namespace :v1 do
-    resources :customers
+    resources :customers do
+      scope module: 'customers' do
+
+        resources :customer_subscriptions
+      end
+
+    end
     end
   end
 end
