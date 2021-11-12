cf309a51ad3081f5b6a97dbc420e9434dd629016
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-11-11 23:13:37 -0500

    Create update action on controller with strong params

--- app/controllers/api/v1/customers/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customers/customer_subscriptions_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::Customers::CustomerSubscriptionsController < ApplicationController
 
   def index
-    customer_subscriptions = Customer.find(params[:customer_id]).customer_subscriptions
+    customer = Customer.find(params[:customer_id])
+    customer_subscriptions = customer.customer_subscriptions if customer
+
     render json: CustomerSubscriptionsSerializer.new(customer_subscriptions), status: :ok
   end
 
@@ -10,7 +12,16 @@ class Api::V1::Customers::CustomerSubscriptionsController < ApplicationControlle
     tea = Tea.find(params[:tea_id])
     subscription = Subscription.find(params[:subscription_id])
     customer_subscription = customer.customer_subscriptions.create!(customer_subscription_params)
+
     render json: CustomerSubscriptionsSerializer.new(customer_subscription), status: :created
+  end
+
+  def update
+    customer = Customer.find(params[:customer_id])
+    customer_subscription = CustomerSubscription.find(params[:id])
+
+    customer_subscription.update!(customer_subscription_params)
+    render json: CustomerSubscriptionsSerializer.new(customer_subscription), status: :ok
   end
 
   private
