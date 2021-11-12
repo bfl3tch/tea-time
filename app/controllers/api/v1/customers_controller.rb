cfdd3ef5ff1a2ea14e073478de39fc397fd13111
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-11-11 20:14:51 -0500

    Create index action for customers controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::CustomersController < ApplicationController
 
   def index
+    customers = Customer.all
+
+    render json: CustomersSerializer.new(customers), status: :ok
   end
 end
