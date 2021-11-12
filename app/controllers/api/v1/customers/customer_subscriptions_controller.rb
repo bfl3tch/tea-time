bb0a0af26a1bd956819da83c475094f764955f4d
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-11-11 22:01:53 -0500

    Use strong params to whitelist create action build on customer subscriptions

--- app/controllers/api/v1/customers/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customers/customer_subscriptions_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::Customers::CustomerSubscriptionsController < ApplicationControlle
     customer_subscriptions = Customer.find(params[:customer_id]).customer_subscriptions
     render json: CustomerSubscriptionsSerializer.new(customer_subscriptions), status: :ok
   end
+
+  def create
+    customer = Customer.find(params[:customer_id])
+    tea = Tea.find(params[:tea_id])
+    subscription = Subscription.find(params[:subscription_id])
+    customer_subscription = customer.customer_subscriptions.create!(customer_subscription_params)
+    render json: CustomerSubscriptionsSerializer.new(customer_subscription), status: :created
+  end
+
+  private
+
+  def customer_subscription_params
+    params.permit(:customer_id, :tea_id, :subscription_id, :active)
+  end
 end
